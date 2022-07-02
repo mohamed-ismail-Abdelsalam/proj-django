9528b6ec69dc10f5c610a838b2e94b4a8def6bd1
Shawwky <shawke1235@yahoo.com>
2022-07-03 00:15:05 +0200

    Raiser Module

---
+++ crowd/Users/userurls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from Users.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+
+    path('register/', register),
+    path('login/', login),
+    path('home/', home, name="home"),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+         activate, name='activate'),
+]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
